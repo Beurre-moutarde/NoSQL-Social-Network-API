66821b86b9f74d0b3fa1bab4df68e9d05cb861a9
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-03-31 12:43:17 +0200

    added User model/schema

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userChema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        },
+        thoughts: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema
+ .virtual('friendCount')
+ .get(function() {
+    return this.friends.lentgh
+ });
