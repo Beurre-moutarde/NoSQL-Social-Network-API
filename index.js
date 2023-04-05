ae346b50dbb202fba310c47ca8bff111ccf81823
Beurre-moutarde <christopher.m.lebreton@gmail.com>
2023-04-05 23:22:20 +0200

    changed index and toughtRoutes

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const cwd = process.cwd();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-mongoose.set('debug', true);
 
 db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`API server for ${activity} running on port ${PORT}!`);
+      console.log(`API server running on port ${PORT}!`);
     });
   });
+
